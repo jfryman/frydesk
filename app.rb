fb273458d61d8b91d594d3eb57db94c0cc3424cb
James Fryman <james@fryman.io>
2016-12-08 15:37:56 -0600

    adding Sinatra, to_i

--- app.rb
+++ app.rb
@@ -12,21 +12,24 @@ require 'rpi_gpio'
 # )
 
 # Setup Relays
+UP_PIN = ENV['UP_GPIO_PIN'].to_i
+DOWN_PIN = ENV['DOWN_GPIO_PIN'].to_i
+
 RPi::GPIO.set_numbering :bcm
-RPi::GPIO.setup ENV['UP_GPIO_PIN'],       :as => :output
-RPi::GPIO.setup ENV['DOWN_GPIO_PIN'],     :as => :output
+RPi::GPIO.setup UP_PIN,   :as => :output
+RPi::GPIO.setup DOWN_PIN, :as => :output
 
 serialport = Serial.new ENV['DISTANCE_DEVICE']
 
 # Test Device Goes Up 5 seconds
-RPi::GPIO.set_high ENV['UP_GPIO_PIN']
+RPi::GPIO.set_high UP_PIN
 sleep 5
-RPi::GPIO.set_low ENV['UP_GPIO_PIN']
+RPi::GPIO.set_low UP_PIN
 
 # Test Device goes down 5 seconds
-RPi::GPIO.set_high ENV['DOWN_GPIO_PIN']
+RPi::GPIO.set_high DOWN_PIN
 sleep 5
-RPi::GPIO.set_low ENV['DOWN_GPIO_PIN']
+RPi::GPIO.set_low DOWN_PIN
 
 puts "Done"
 sleep 9999999

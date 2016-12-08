c61df0fbcaf4fcb936985eb2b9492cc6ecb9452c
James Fryman <james@fryman.io>
2016-12-08 15:16:21 -0600

    only serial port for now

--- app.rb
+++ app.rb
@@ -1,20 +1,20 @@
 require 'rubygems'
 require 'rubyserial'
-require 'rpi_gpio'
-require 'mqtt'
+# require 'rpi_gpio'
+# require 'mqtt'
 
-client = MQTT::Client.connect(
-  :host     => ENV['MQTT_HOST'],
-  :port     => 8883,
-  :ssl      => true,
-  :username => ENV['MQTT_USER'],
-  :password => ENV['MQTT_PASSWORD']
-)
+# client = MQTT::Client.connect(
+#   :host     => ENV['MQTT_HOST'],
+#   :port     => 8883,
+#   :ssl      => true,
+#   :username => ENV['MQTT_USER'],
+#   :password => ENV['MQTT_PASSWORD']
+# )
 
 # Setup Relays
-RPi::GPIO.set_numbering :bcm
-RPi::GPIO.setup ENV['UP_GPIO_PIN'],       :as => :output
-RPi::GPIO.setup ENV['DOWN_GPIO_PIN'],     :as => :output
+# RPi::GPIO.set_numbering :bcm
+# RPi::GPIO.setup ENV['UP_GPIO_PIN'],       :as => :output
+# RPi::GPIO.setup ENV['DOWN_GPIO_PIN'],     :as => :output
 
 serialport = Serial.new '/dev/ttyAMA0'
 

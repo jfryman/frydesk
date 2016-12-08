728d56c136c27bf0104da098645d388daf6e6166
James Fryman <james@fryman.io>
2016-12-08 14:02:47 -0600

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'mqtt',     '~> 0.4.0'
+gem 'rpi_gpio', '~> 0.3.2'
---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'rubyserial'
+require 'rpi_gpio'
+require 'mqtt'
+
+client = MQTT::Client.connect(
+  :host     => ENV['MQTT_HOST'],
+  :port     => 8883,
+  :ssl      => true,
+  :username => ENV['MQTT_USER'],
+  :password => ENV['MQTT_PASSWORD']
+)
+
+# Setup Relays
+RPi::GPIO.set_numbering :bcm
+RPi::GPIO.setup ENV['UP_GPIO_PIN'],       :as => :output
+RPi::GPIO.setup ENV['DOWN_GPIO_PIN'],     :as => :output
+
+puts Dir['/dev']
